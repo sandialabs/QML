8864d676394d4267ee6acd30225b4e7b6745b425
Rushil Dandamudi <rcdanda@sandia.gov>
2024-08-04 22:57:43 -0700

    documenting analyze script and removing large files

--- qml_serial_analyze.py
+++ qml_serial_analyze.py
@@ -6,6 +6,10 @@ import pickle
 from data.crowley.conv_images import image_from_vector
 from qml_serial_preprocess import initialize
 
+# Main visualization logic: displays each of nProp propagations starting from the START_IDX-th image
+# SHAPE is the shape of each image: image_size = SHAPE[0] * SHAPE[1]
+# x is the original, un-normalized condensed dataset matrix: N x image_size
+# peak_idxs is the same as in qml_serial --- the propagation images for each start image: [nProp x nColl] x N
 def visualize_propagations(x, peak_idxs, nProp, nColl, START_IDX, SHAPE):
     curr_props = peak_idxs[START_IDX]
     print(curr_props)
@@ -19,6 +23,7 @@ def visualize_propagations(x, peak_idxs, nProp, nColl, START_IDX, SHAPE):
             assert x[point].shape[0] == SHAPE[0] * SHAPE[1], 'Incorrect shape for image vector'
             image_from_vector(x[point], SHAPE)
 
+# retrieves the latest directory in the results directory
 def last_result():
     all_result_dirs = glob.glob(os.path.join(RESULTS_DIR, '*'))
     return max(all_result_dirs, key=os.path.getctime)
@@ -55,17 +60,18 @@ if __name__ == '__main__':
         nProp = qml_params['nProp']
         nColl = qml_params['nColl']
 
+        # gets the shape of the image
         with open(os.path.join(dir_name, 'shape.txt'), 'r') as f:
             shape_lines = f.readlines()
             SHAPE = (int(shape_lines[0]), int(shape_lines[1]))        
         
-
+        # gets the condensed dataset
         with open(os.path.join(dir_name, 'x.pickle'), 'rb') as f:
             x = pickle.load(f)
 
+        # gets the propagation information
         with open(os.path.join(dir_name, 'peak_idxs.pickle'), 'rb') as f:
             peak_idxs = pickle.load(f)
-        
 
         visualize_propagations(x, peak_idxs, nProp, nColl, start_idx, SHAPE)
 
